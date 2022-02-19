6ca1b640d24a3d4be319ee24c278d580ddf8c44c
PlatinumMaster <L33TGamer@outlook.com>
2022-02-19 01:49:21 -0500

    Update matrix structure.

--- Formats/Maps/MapMatrix.cs
+++ Formats/Maps/MapMatrix.cs
@@ -6,6 +6,12 @@ namespace BeaterLibrary.Formats.Maps {
         public bool includeMapHeaders { get; set; }
         public List<MapMatrixRow> mapFilesMatrix { get; set; }
         public List<MapMatrixRow> mapHeadersMatrix { get; set; }
+        
+        public MapMatrix() {
+            mapFilesMatrix = new List<MapMatrixRow>();
+            mapHeadersMatrix = new List<MapMatrixRow>();
+        }
+        
         public MapMatrix(byte[] data) {
             BinaryReader binary = new BinaryReader(new MemoryStream(data));
             mapFilesMatrix = new List<MapMatrixRow>();
@@ -29,6 +35,24 @@ namespace BeaterLibrary.Formats.Maps {
                     mapHeadersMatrix.Add(entries);
                 }
             }
+        }
+
+        public void serialize(string path) {
+            BinaryWriter binary = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate));
+            binary.Write((uint)(includeMapHeaders ? 1 : 0));
+            if (mapFilesMatrix.Count > 0) {
+                binary.Write((ushort) mapFilesMatrix[0].Count);
+            }
+            binary.Write((ushort)mapFilesMatrix.Count);
+            mapFilesMatrix.ForEach(x => {
+                x.ForEach(e => binary.Write(e.value));
+            });
+            if (includeMapHeaders) {
+                mapHeadersMatrix.ForEach(x => {
+                    x.ForEach(e => binary.Write(e.value));
+                });
+            }
+            binary.Close();
         }
     }
 }
