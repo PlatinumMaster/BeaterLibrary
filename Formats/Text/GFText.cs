a62c5fc5e1290a0207e9379e213832acaea1a251
PlatinumMaster <l33tgamer@outlook.com>
2023-05-31 01:20:34 -0400

    Script & Text editors now load and save data (again).

--- Formats/Text/GFText.cs
+++ Formats/Text/GFText.cs
@@ -8,7 +8,7 @@ using BeaterLibrary.Parsing;
 namespace BeaterLibrary.Formats.Text {
     public class GFText {
         private List<GFString> _strings;
-        private const int defaultStep = 0x3;
+        private const int DefaultStep = 0x3;
 
         public List<GFString> strings {
             get => _strings;
@@ -17,11 +17,11 @@ namespace BeaterLibrary.Formats.Text {
         public GFText(byte[] data) {
             _strings = new List<GFString>();
             using (BinaryReader b = new BinaryReader(new MemoryStream(data))) {
-                parseTextContainer(b);
+                ParseTextContainer(b);
             }
         }
 
-        private void parseTextContainer(BinaryReader b) {
+        private void ParseTextContainer(BinaryReader b) {
             var tableOffsets = new List<uint>();
             var characterCounts = new List<ushort>();
             var unknown2 = new List<ushort>();
@@ -44,11 +44,11 @@ namespace BeaterLibrary.Formats.Text {
                 for (int Index = 0; Index < characterCounts[index]; ++Index) {
                     characters.Add((char) b.ReadUInt16());
                 }
-                _strings.Add(new GFString(characters, (ushort) (defaultStep + index)));
+                _strings.Add(new GFString(characters, (ushort) (DefaultStep + index)));
             });
         }
 
-        public string fetchTextAsString(bool includeComments, bool beautify) {
+        public string FetchTextAsString(bool includeComments, bool beautify) {
             StringBuilder s = new StringBuilder();
             for (int Index = 0; Index < strings.Count; ++Index) {
                 if (includeComments) {
@@ -71,7 +71,7 @@ namespace BeaterLibrary.Formats.Text {
             return s.ToString();
         }
 
-        public List<string> fetchTextAsStringArray() {
+        public List<string> FetchTextAsStringArray() {
             List<string> s = new List<string>();
             for (int Index = 0; Index < strings.Count; ++Index) {
                 s.Add(strings[Index].ToString());
@@ -79,15 +79,15 @@ namespace BeaterLibrary.Formats.Text {
             return s;
         }
 
-        public void serialize(string text, string output) {
-            var binary = new BinaryWriter(File.Open(Path.GetFullPath(output), FileMode.OpenOrCreate));
+        public void Serialize(string text, string output) {
+            var binary = new BinaryWriter(File.Open(output, FileMode.OpenOrCreate));
             var characterCounts = new List<ushort>();
             _strings = new List<GFString>();
 
             int textIndex = 0;
             new TextTokenizer().tokenize(text).ForEach(x => {
                 List<AbstractSyntaxNode> importantTokens = x.FindAll(y => y.type is TextTokens.CompressionFlag or TextTokens.TextCharacter or TextTokens.ControlCharacter or TextTokens.ByteSequence);
-                strings.Add(new GFString(importantTokens, (ushort)(defaultStep + textIndex++)));
+                strings.Add(new GFString(importantTokens, (ushort)(DefaultStep + textIndex++)));
             });
 
             strings.ForEach(x => characterCounts.Add(x.charCount));
